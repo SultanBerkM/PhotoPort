81701b74cd81afa63f594909585fac42b6c9c260
Berk <berk.giyunay@gmail.com>
2025-09-03 12:29:08 +0300

    Minor changes on Dashboard and strucutre

--- src/components/Cards/StatiticsCards.jsx
+++ src/components/Cards/StatiticsCards.jsx
@@ -1,11 +1,13 @@
-function StatisticsCards({pathData, cardTitle, numberData}) {
-    return(
-        <div className="border-2 rounded-2xl w-full h-auto m-10 p-5 flex flex-col items-start justify-center gap-5 border-gray-400">
-            <svg xmlns="http://www.w3.org/2000/svg" height="45px" viewBox="0 -960 960 960" width="45px" fill="#4D4D4D">
-            <path d={pathData}/></svg>
-            <h1 className="text-4xl">{cardTitle}</h1>
-            <h2 className="text-6xl text-blue-500">{numberData}</h2>
-        </div>
-    )
+function StatisticsCards({ pathData, cardTitle, numberData }) {
+  return (
+    <div className="border-2 rounded-2xl w-full sm:w-[48%] lg:w-[22%] h-auto m-4 p-5 flex flex-col items-start justify-center gap-5 border-gray-400 shadow-sm">
+      <svg xmlns="http://www.w3.org/2000/svg" height="45px" viewBox="0 -960 960 960" width="45px" fill="#4D4D4D">
+        <path d={pathData} />
+      </svg>
+      <h1 className="text-2xl xl:text-4xl">{cardTitle}</h1>
+      <h2 className="text-4xl xl:text-6xl text-blue-500">{numberData}</h2>
+    </div>
+  );
 }
+
 export default StatisticsCards;
--- src/pages/UserDashboard/MainDashboad.jsx
+++ src/pages/UserDashboard/MainDashboad.jsx
@@ -13,12 +13,16 @@ function MainDashboard(){
                    <UploadBtn />
                </div>
             </div>
-            
-            <div className="flex h-1/4 w-full items-center justify-center">
+            <hr className="border-gray-300 border-1 mt-4"/>
+            <div className="flex flex-wrap justify-center items-stretch w-full">
                <StatisticsCards cardTitle={"Uploaded Works"} numberData={132} pathData={"M240-280h480L570-480 450-320l-90-120-120 160Zm-80 160q-33 0-56.5-23.5T80-200v-480q0-33 23.5-56.5T160-760h126l74-80h240l74 80h126q33 0 56.5 23.5T880-680v480q0 33-23.5 56.5T800-120H160Zm0-80h640v-480H638l-73-80H395l-73 80H160v480Zm320-240Z"} />
                <StatisticsCards cardTitle={"Upcoming Bookings"} numberData={3} pathData={"M200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Z"}/>
                <StatisticsCards cardTitle={"Rating"} numberData={4.8} pathData={"m305-704 112-145q12-16 28.5-23.5T480-880q18 0 34.5 7.5T543-849l112 145 170 57q26 8 41 29.5t15 47.5q0 12-3.5 24T866-523L756-367l4 164q1 35-23 59t-56 24q-2 0-22-3l-179-50-179 50q-5 2-11 2.5t-11 .5q-32 0-56-24t-23-59l4-165L95-523q-8-11-11.5-23T80-570q0-25 14.5-46.5T135-647l170-57Zm49 69-194 64 124 179-4 191 200-55 200 56-4-192 124-177-194-66-126-165-126 165Zm126 135Z"}/>
                <StatisticsCards cardTitle={"New Messages"} numberData={5} pathData={"M80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"}/>
+            </div>
+
+            <div>
+                <h2>Recent Uploads</h2>
             </div>
             
         </div>  
